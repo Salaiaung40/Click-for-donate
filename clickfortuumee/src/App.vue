2bd70e255c77f36dc92afd859f14e1f983b60f18
Salaiaung40 <salaihtunlin@hotnail.com>
2022-08-24 16:45:44 +0200

    First commit

---
+++ clickfortuumee/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+  outline: 0px;
+  box-sizing: border-box;
+  font-size: 100%;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  /* background-color: rgba(187, 179, 159, 0.925); */
+  background: rgba(187, 179, 159, 0.925);
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ clickfortuumee/src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="body">
+    <div class="header">
+      <h1>{{ msg }}</h1>
+      <h2>Essential Links</h2>
+    </div>
+    <div class="but-grup">
+      <Buttons-Compo
+        onclick="window.location.href = 'https://salai.be/';"
+        target="'_blank';"
+      >
+      </Buttons-Compo>
+
+      <Buttons-Compo></Buttons-Compo>
+      <Buttons-Compo></Buttons-Compo>
+      <Buttons-Compo onclick="window.open('https://salai.be/','_blank')">
+      </Buttons-Compo>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonsCompo from "@/components/ButtonsCompo.vue";
+export default {
+  name: "Home",
+  components: {
+    "Buttons-Compo": ButtonsCompo
+  },
+  data() {
+    return {
+      msg: "Welcome to  Click for TuuMee"
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* template {
+  background-color: rgba(187, 179, 159, 0.925);
+} */
+.header {
+  padding-top: 2vh;
+}
+.but-grup {
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  width: 80%;
+  /* border: 3px solid green; */
+  padding: 10px;
+}
+</style>
